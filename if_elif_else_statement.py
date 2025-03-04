8634b4676e36c005285937dfccabae34189bcd75
paul12550 <>
2025-03-04 13:27:17 +0800

    Last Sync: 2025-03-04 05:27:17 (Mobile)

--- if_elif_else_statement.py
+++ if_elif_else_statement.py
@@ -1,17 +1,19 @@
-user_input = input("Enter your numeric grade: ")
-grade = int(user_input)
-if grade >= 90:
-    letter_grade = "A"
-elif grade >= 80:
-    letter_grade = "B"
-elif grade >= 70:
-    letter_grade = "C"
-elif grade >= 60:
-    letter_grade = "D"
-else:
-    letter_grade = "F"
-print("Your grade is:", letter_grade)
 try:
-    #your code here
+    user_input = input("Enter your numeric grade: ")
+    grade = int(user_input)
+    
+    if grade >= 90:
+        letter_grade = "A"
+    elif grade >= 80:
+        letter_grade = "B"
+    elif grade >= 70:
+        letter_grade = "C"
+    elif grade >= 60:
+        letter_grade = "D"
+    else:
+        letter_grade = "F"
+    
+    print("Your grade is:", letter_grade)
+    
 except ValueError:
     print("Invalid input. Please enter an integer.")
